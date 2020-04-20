e54770b949421619b8a67004e714d5c568420519
Omid2121 <Omid_Ahangi@github.com>
2020-04-20 14:49:56 -0700

    Lille bank OOP

---
+++ Bank/Bank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Tobias", 2000);
+            Console.WriteLine($"Konto {account.Number} blev oprettet til {account.Owner} med {account.Balance}. ");
+
+            account.MakeWithdrawal(200, DateTime.Now, "Stol");
+            Console.WriteLine("Penge tilbage: "+ account.Balance);
+        }
+    }
+}
